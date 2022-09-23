f8491274fd73a4b61831018fb711e269aa17d105
msroji <89507971+msroji@users.noreply.github.com>
2022-09-23 15:45:43 +0100

    Update challenge_23.py
    group your inputs into a procedure and then call the subroutines under main method

--- basic challenges/challenge_23.py
+++ basic challenges/challenge_23.py
@@ -1,14 +1,18 @@
-length = float(input("Enter the length of the garden in metres: "))
-width = float(input("Enter the width of the garden in metres: "))
-radius = float(input("Enter the radius of the circular flower bed in meters: "))
+def userinput():
+    length = float(input("Enter the length of the garden in metres: "))
+    width = float(input("Enter the width of the garden in metres: "))
+    radius = float(input("Enter the radius of the circular flower bed in meters: "))
+    return length,width,radius
 
 def flower_bed(radius):
     return (radius**2)*3.14
-circle_area = flower_bed(radius)
 
-def garden():
+def garden(length,width):
     return length * width
-garden_area = garden()
 
-turf_needed = garden_area-circle_area
-print(f"{turf_needed} metres")
+if __name__ == "__main__":
+    length,width,radius = userinput()
+    circle_area = flower_bed(radius)
+    garden_area = garden()
+    turf_needed = garden_area-circle_area
+    print(f"{turf_needed} metres")
